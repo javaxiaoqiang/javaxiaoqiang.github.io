74a7e93306c5b730ba82f86a80f34ff3fd303454
强少 <1576585012@qq.com>
2025-11-06 09:25:57 +0800

    修改vueconfig

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/javaxiaoqiang.github.io/'  // 替换为你的仓库名
+//     : '/'
+// }
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/javaxiaoqiang.github.io/'  // 替换为你的仓库名
-    : '/docs'
+  publicPath: '/'
 }
